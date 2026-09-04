2ca30cf00684c23bb6e9f5a39fc75b0cfef42c83
lingxi <lxy2005051020@gmail.com>
2026-09-04 17:09:20 +0800

    feat: integrate state lifecycle system

--- sgs_v2/battle_core/battle_systems.py
+++ sgs_v2/battle_core/battle_systems.py
@@ -8,6 +8,7 @@ from .action_system import ActionSystem
 from .attribute_system import AttributeSystem
 from .damage_system import DamageSystem
 from .normal_attack_system import NormalAttackSystem
+from .state_lifecycle_system import StateLifecycleSystem
 from .target_system import TargetSystem
 from .troop_system import TroopSystem
 from .victory_system import VictorySystem
@@ -15,7 +16,7 @@ from .victory_system import VictorySystem
 
 @dataclass(slots=True)
 class BattleSystems:
-    """阶段 2 BattleSystem 组合根，供 BattleEngine 使用。"""
+    """BattleSystem 组合根，供 BattleEngine 使用。"""
 
     attribute_system: AttributeSystem = field(default_factory=AttributeSystem)
     target_system: TargetSystem = field(default_factory=TargetSystem)
@@ -29,6 +30,10 @@ class BattleSystems:
     strategy_troop_function_table: Mapping[int, int] | None = None
     strategy_random_percent_range: tuple[int, int] = (86, 94)
     strategy_low_damage_floor_range: tuple[int, int] = (5, 15)
+
+    state_lifecycle_system: StateLifecycleSystem = field(
+        default_factory=StateLifecycleSystem
+    )
 
     action_order_system: ActionOrderSystem = field(init=False)
     damage_system: DamageSystem = field(init=False)
