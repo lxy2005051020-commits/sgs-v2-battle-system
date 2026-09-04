974d8f1b693a6a452064b6fe3c31acb10fe2a7b2
lingxi <lxy2005051020@gmail.com>
2026-09-04 23:01:20 +0800

    feat: export stage5 effect APIs

--- sgs_v2/battle_core/__init__.py
+++ sgs_v2/battle_core/__init__.py
@@ -15,7 +15,27 @@ from .action_order_system import ActionOrderSystem
 from .action_system import ActionSystem
 from .attribute_system import AttributeSystem, AttributeModifierProvider
 from .battle_systems import BattleSystems
+from .damage_resolution_system import (
+    DamageResolutionResult,
+    DamageResolutionSystem,
+)
 from .damage_system import DamageRequest, DamageResult, DamageSystem
+from .effect_executor import EffectExecutor
+from .effect_result import (
+    ApplyStateEffectResult,
+    DamageEffectResult,
+    DeferredEffectResult,
+    EffectExecutionResult,
+    EffectExecutionStatus,
+    RemoveStateEffectResult,
+)
+from .effects import (
+    ApplyStateEffect,
+    DamageEffect,
+    Effect,
+    RecoverEffect,
+    RemoveStateEffect,
+)
 from .normal_attack_system import NormalAttackResult, NormalAttackSystem
 from .official_state_catalog import (
     OFFICIAL_STATE_CATALOG,
@@ -28,6 +48,10 @@ from .state_definition import StateDefinition
 from .state_instance import StateInstance
 from .state_lifecycle_system import StateLifecycleSystem
 from .state_registry import StateRegistry
+from .state_runtime_params import (
+    EmptyStateRuntimeParams,
+    StateRuntimeParams,
+)
 from .strategy_damage_formula import StrategyBaseDamageFormula
 from .target_system import TargetSystem
 from .troop_system import TroopChangeResult, TroopSystem
@@ -54,9 +78,23 @@ __all__ = [
     "AttributeSystem",
     "AttributeModifierProvider",
     "BattleSystems",
+    "DamageResolutionResult",
+    "DamageResolutionSystem",
     "DamageRequest",
     "DamageResult",
     "DamageSystem",
+    "EffectExecutor",
+    "EffectExecutionStatus",
+    "EffectExecutionResult",
+    "DamageEffectResult",
+    "ApplyStateEffectResult",
+    "RemoveStateEffectResult",
+    "DeferredEffectResult",
+    "Effect",
+    "DamageEffect",
+    "ApplyStateEffect",
+    "RemoveStateEffect",
+    "RecoverEffect",
     "NormalAttackResult",
     "NormalAttackSystem",
     "OFFICIAL_STATE_CATALOG",
@@ -68,6 +106,8 @@ __all__ = [
     "StateInstance",
     "StateLifecycleSystem",
     "StateRegistry",
+    "StateRuntimeParams",
+    "EmptyStateRuntimeParams",
     "StrategyBaseDamageFormula",
     "TargetSystem",
     "TroopChangeResult",
